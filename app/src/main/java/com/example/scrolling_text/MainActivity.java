a6573c4303484b5c03b9b7be868cd48adcff0b16
Alfons Lorente Darder <alorente@cifpfbmoll.com>
2021-10-24 12:17:26 +0200

    Text try

--- app/src/main/java/com/example/scrolling_text/MainActivity.java
+++ app/src/main/java/com/example/scrolling_text/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.scrolling_text;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String FILENAME = "app/src/main/res/text/article.txt";
+    private String mArticleText;
+    TextView mArticle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mArticleText = getArticle();
+        mArticle = (TextView) findViewById(R.id.article);
+        mArticle.setText(mArticleText);
+
+    }
+
+    private String getArticle() {
+        String text = "";
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILENAME))) {
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                text = text + " " + line;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
     }
 }
