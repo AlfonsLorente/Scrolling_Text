2e9b89bc45b039a1916e431c79072a4b114affaa
Alfons Lorente Darder <alorente@cifpfbmoll.com>
2021-10-31 01:04:36 +0200

    Almost done

---
+++ app/src/main/java/com/example/scrolling_text/CommentaryInput.java
@@ -0,0 +1,45 @@
+package com.example.scrolling_text;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class CommentaryInput extends AppCompatActivity {
+    private Button mSendButton;
+    public static final String EXTRA_MESSAGE = "com.example.scrolling_text.MESSAGE";
+    private static String mMessage;
+    private TextView mCommentaryText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commentary_input);
+        mCommentaryText = (TextView) findViewById(R.id.CommentaryText);
+        mSendButton = (Button) findViewById(R.id.sendButton);
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //EditText CommentaryText = (EditText) findViewById(R.id.CommentaryText);
+                //mMessage = CommentaryText.getText().toString();
+                Intent intent = new Intent(CommentaryInput.this, MainActivity.class);
+                mMessage = mCommentaryText.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, mMessage);
+
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+
+        });
+
+    }
+
+    public static String getMessage(){
+        return mMessage;
+    }
+
+
+}
--- app/src/main/java/com/example/scrolling_text/MainActivity.java
+++ app/src/main/java/com/example/scrolling_text/MainActivity.java
@@ -2,42 +2,101 @@ package com.example.scrolling_text;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String FILENAME = "app/src/main/res/text/article.txt";
-    private String mArticleText;
-    TextView mArticle;
-
+    //private static final String FILENAME = "app/src/main/res/raw/article.txt";
+    //private StringBuffer mArticleText;
+    //TextView mArticle;
+    private static final String KEY_INDEX = "index";
+    private LinearLayout linearLayout;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mArticleText = getArticle();
-        mArticle = (TextView) findViewById(R.id.article);
-        mArticle.setText(mArticleText);
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        linearLayout = (LinearLayout)findViewById(R.id.linearLayoutTextView);
+        //mArticleText = getArticle();
+
+        if(counter > 0) {
+            createTextView();
+        }
+        //mArticle = (TextView) findViewById(R.id.article);
+        //mArticle.setMovementMethod(new ScrollingMovementMethod());
+        //mArticle.setText(mArticleText);
+
 
     }
 
-    private String getArticle() {
-        String text = "";
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILENAME))) {
+    private void createTextView() {
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(CommentaryInput.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setText("Commentary: " + "\n" + message + "\n");
+        linearLayout.addView(textView);
 
-            String line;
+    }
 
-            while ((line = bufferedReader.readLine()) != null) {
-                text = text + " " + line;
+
+    public void writeCommentary(View view) {
+        counter++;
+        Intent intent = new Intent(this, CommentaryInput.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(KEY_INDEX, counter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Pene", "Me mueroooo");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Pene", "Me pausooo");
+    }
+
+/*
+    private StringBuffer getArticle() {
+        StringBuffer stringBuffer = new StringBuffer();
+        InputStream inputStream = this.getResources().openRawResource(R.raw.article);
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+            String data;
+
+            while ((data = bufferedReader.readLine()) != null) {
+                stringBuffer.append(data + "\n");
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return text;
+        return stringBuffer;
+
     }
+    */
+
 }
